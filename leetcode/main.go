7902cfb5011c4985feb45433d50c4e275b805f4c
RofiAlfarid <RofiAlfarid>
2022-12-20 21:23:45 +0700

    just add

---
+++ leetcode/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/leetcode/voids"
+)
+
+func main() {
+	input := []int{1, 2}
+	x := voids.PivotIndex(input)
+	// voids.PivotIndex()
+	log.Println(x)
+}
